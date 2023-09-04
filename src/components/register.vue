1380235f26e08d466dae031cfe1814da07b73fa0
Amy <dingxiaoyu168@outlook.com>
2023-09-04 22:36:45 +0800

    美化登录和注册组件样式

--- src/components/register.vue
+++ src/components/register.vue
@@ -1,22 +1,14 @@
 <template>
-  <el-dialog v-model="dialogVisible" width="45%" align-center="true" style="height:59%;">
-    <div class="contnet">
-      <span class="dialog-footer">
-        <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-          <div class="centered-content">
-            <el-form-item prop="pass">
-              <el-input placeholder="账号" type="text" v-model="ruleForm.username" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item prop="checkPass">
-              <el-input placeholder="密码" type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="submitForm('ruleForm')">注册</el-button>
-            </el-form-item>
-          </div>
+  <el-dialog v-model="dialogVisible" width="500px" align-center="true" style="height:550px;">
+    <span class="dialog-footer">
+        <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="custom-form">
+              <div class="register">注册</div>
+              <el-input placeholder="账号" type="text" v-model="ruleForm.username" autocomplete="off" class="input"></el-input>
+              <el-input placeholder="密码" type="password" v-model="ruleForm.password" autocomplete="off" class="input"></el-input>
+              <img src="../assets/notion-parade.webp" class="img">
+              <el-button class="registerButton" type="primary" @click="submitForm('ruleForm')">注册</el-button>
         </el-form>
-      </span>
-    </div>
+    </span>
   </el-dialog>
 </template>
   
@@ -91,66 +83,68 @@ export default {
 </script>
 
 <style>
-.content {
-  width: 100%;
-  text-align: center; /* 居中文本 */
-}
-
-.centered-content {
-  display: inline-block; /* 让内部元素水平居中 */
-  text-align: left; /* 内部元素左对齐 */
-}
-.img{
-  width:307px;
-}
-.demo-ruleForm{
-  width:100%;
-  height:100%;
-}
-.el-form {
+.custom-form {
+  width: 70%!important;
+  left:35%;
   text-align: center;
   background-color: #fff;
-  width: 50%;
-  height: 50%;  
+  height: 50%;
+  position: absolute;
+  left: 50%!important;
+  top: 35%;
   transform: translate(-50%, -50%);
 }
-.el-form-item{
-  width:409px;
+.register{
+  text-align: center; /* 水平居中 */
+  display: flex;
+  justify-content: center; /* 水平居中 */
+  align-items: center; /* 垂直居中 */
+  margin-bottom: 37px;
+  color: #000;
+  font-family: Inter;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 24px; /* 100% */
+  letter-spacing: -0.13px;
+  height: 10%; /* 设置一个高度，以便垂直居中 */
+}
+
+.img{
+  padding-left:0!important;
+  width:240px!important;
+}
+.el-input__wrapper{
+  width:400px;
   height:64px;
 }
 .input{
-  width:409px;
-  height:64px;
-  border-radius: 12px;
-  border: 1px solid #FFF;
-  background: #F7F7F4;
-  color: #A7A7A6;
+  margin-bottom:37px;
 }
-
-.el-input__inner{
-  display:flex;
-  align-items: center;
-  width:409px;
-  height:64px;
-  border-radius: 12px;
-  border: 1px solid #FFF;
-  background: #F7F7F4;
+.el-input__inner {
   color: #A7A7A6;
-  font-family: Inter;
   font-size: 24px;
   font-style: normal;
   font-weight: 500;
   line-height: 24px; /* 100% */
   letter-spacing: -0.13px;
 }
+.button-container {
+  text-align: center; /* 水平居中 */
+  display: flex;
+  justify-content: center; /* 水平居中 */
+  align-items: center; /* 垂直居中 */
+  height: 100%; /* 设置适当的高度，确保按钮在容器内居中 */
+}
+
 </style>
 
 <style scoped>
 .dialog-footer button:first-child {
   margin-right: 10px;
 }
-.loginButton{
-  width:409px;
+.registerButton{
+  width:350px;
   height:64px;
   color: #E94457;
   font-family: Inter;
@@ -162,5 +156,9 @@ export default {
   border-radius: 12px;
   border: 1px solid #F8DAD7;
   background: #FDF5F2;
+  text-align: center; /* 水平居中 */
+  display: flex;
+  justify-content: center; /* 水平居中 */
+  align-items: center; /* 垂直居中 */
 }
 </style>
