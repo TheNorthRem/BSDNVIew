3abee259c2fd1faf62ef8b6f3362bab11fff1225
Jungle <1239946358@qq.com>
2023-09-07 14:16:16 +0800

    增加token核验功能

--- src/views/bsHome.vue
+++ src/views/bsHome.vue
@@ -64,7 +64,7 @@
 import { ElCarousel, ElCarouselItem, ElBacktop} from '@/../node_modules/element-plus';
 import {Created} from "vue"
 import showEditor_brief from '@/components/showEditor_brief.vue';
-import { getUserInfo } from '../http/api.js';
+import { getUserInfo, getToken } from '../http/api.js';
 import { getTopArticles } from "@/http/api"
 
 export default { 
@@ -98,14 +98,14 @@ export default {
         getTopArticles().then(res => {
                 if (res.data.code == 200) {
                     console.log("获取热门文章成功！")
-                    console.log(res.data.data)
+                    //console.log(res.data.data)
                     this.articleArrayLength = res.data.data.length
                     this.TopArticles = res.data.data
 
                     //修改简介格式
                     for(let i = 0; i < this.TopArticles.length; i++) {
                         this.TopArticles[i].brief = "<h3>简介：".concat(this.TopArticles[i].brief).concat("......</h3>")
-                        console.log(this.TopArticles[i].brief)
+                        //console.log(this.TopArticles[i].brief)
                     }
                     // this.TopArticles.brief = String(this.TopArticles.brief).concat("...")
                     // console.log(this.TopArticles)
@@ -120,27 +120,34 @@ export default {
     methods:{
         Reload() {
             //判断用户的登录状态，如果用户已经登录，则显示用户的头像和个人信息
-            //TODO:Redis token校验
-            let userID=localStorage.getItem('ID');//获取用户ID
+            //token校验
+            let userID = localStorage.getItem('ID');//获取用户ID
+            let token = localStorage.getItem('token');
+            if(userID == null || token == null) return;
             let IDForm = {
                 id: userID,
             }
+            console.log(IDForm)
             console.log("userID",userID);
-            if(userID == null) return;
-            else{
-                getUserInfo(IDForm) 
-                .then(result => {
-                    console.log(result)
-                    this.userName = result.data.data.userName;
-                    this.nickName = result.data.data.nickName;
-                    this.intro = result.data.data.intro;
-                })
-                .catch(error => {
-                    console.error('获取用户信息失败:', error);
-                });
-            }
+            getToken(IDForm).then(result => {
+                if (result.data.code == 200 && result.data.message == token) {
+                    let NewIDFrom = {
+                        id: userID,
+                    }
+                    getUserInfo(NewIDFrom) .then(result => {
+                        console.log(result)
+                        this.userName = result.data.data.userName;
+                        this.nickName = result.data.data.nickName;
+                        this.intro = result.data.data.intro;
+                    }).catch(error => {
+                        console.error('获取用户信息失败:', error);
+                    });
+                }
+                else {
+                    console.error("获取用户信息失败!");
+                }
+            })
         },
-       
     },
     // async beforeMount() {
     //     await this.getTopArticlesFunction()
